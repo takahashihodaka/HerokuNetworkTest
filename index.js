87764b3c15307be0bcb95a5186d1438b5563b4d6
takahashihodaka <takahashi.hodaka.2107@members.co.jp>
2020-03-27 12:36:30 +0900

    Add: IPアドレス表示

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const portForDev = 4000
+
+app.set('trust proxy', 1)
+
+app.set('port', process.env.PORT || portForDev)
+
+app.disable('x-powered-by')
+
+app.get('/',(req, res) => {
+  res.send('Network Test' + req.ip)
+  console.log(req.ip)
+  console.log(req.ips)
+})
+
+// サーバを立てる
+app.listen(app.get('port'), () => {
+  console.log('■■■ Main server listening on: http://localhost:' + app.get('port'))
+})
